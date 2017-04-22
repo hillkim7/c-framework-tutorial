29a02ef1a2295a3a79a18d7a1cc5d4aa496155d3
Hill <flowtide@naver.com>
2017-04-22 21:07:32 +0900

    Add steady_timer example

--- boost_examples/boost_asio/simple_log.cpp
+++ boost_examples/boost_asio/simple_log.cpp
@@ -2,24 +2,20 @@
  * A simple sample of Boost Log
  */
 
-#pragma warning(push)
-#pragma warning(disable:4819)
-#   include <boost/shared_ptr.hpp>
-#   include <boost/date_time/posix_time/posix_time_types.hpp>
-#   include <boost/log/trivial.hpp>
-#   include <boost/log/core.hpp>
-#   include <boost/log/expressions.hpp>
-#   include <boost/log/sources/logger.hpp>
-#   include <boost/log/utility/setup/file.hpp>
-#   include <boost/log/utility/setup/console.hpp>
-#   include <boost/log/utility/setup/common_attributes.hpp>
-#   include <boost/log/support/date_time.hpp>
-#   include <boost/log/sinks/sync_frontend.hpp>
-#   include <boost/log/sinks/text_file_backend.hpp>
-#   include <boost/log/sinks/text_ostream_backend.hpp>
-#   include <boost/log/attributes/named_scope.hpp>
-#pragma warning(pop)
-#pragma warning(disable:4503)
+#include <boost/shared_ptr.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/attributes/named_scope.hpp>
 
 static void init_log(void)
 {
---
+++ boost_examples/boost_asio/steady_timer.cpp
@@ -0,0 +1,57 @@
+/*
+The asio::steady_timer class is based on the std::chrono::steady_clock class,
+which represents a steady clock that is not influenced by the system clock changes.
+It means that asio::steady_timer is a good choice to measure intervals.
+*/
+
+#include <boost/asio/steady_timer.hpp>
+#include <iostream>
+
+using namespace boost;
+
+int main()
+{
+	asio::io_service ios;
+
+	asio::steady_timer t1(ios);
+	t1.expires_from_now(std::chrono::seconds(2));
+
+	asio::steady_timer t2(ios);
+	t2.expires_from_now(std::chrono::seconds(5));
+
+	t1.async_wait([&t2](boost::system::error_code ec) {
+		if (ec == 0) {
+			std::cout << "Timer #1 has expired!" << std::endl;
+		}
+		else if (ec == asio::error::operation_aborted) {
+			std::cout << "Timer #1 has been canceled!"
+				<< std::endl;
+		}
+		else {
+			std::cout << "Error occurred! Error code = "
+				<< ec.value()
+				<< ". Message: " << ec.message()
+				<< std::endl;
+		}
+		t2.cancel();
+	});
+
+	t2.async_wait([](boost::system::error_code ec) {
+		if (ec == 0) {
+			std::cout << "Timer #2 has expired!" << std::endl;
+		}
+		else if (ec == asio::error::operation_aborted) {
+			std::cout << "Timer #2 has been canceled!"
+				<< std::endl;
+		}
+		else {
+			std::cout << "Error occurred! Error code = "
+				<< ec.value()
+				<< ". Message: " << ec.message()
+				<< std::endl;
+		}
+	});
+	ios.run();
+
+	return 0;
+}
